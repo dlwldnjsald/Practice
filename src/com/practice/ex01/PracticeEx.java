47b95e745a1547a65ff5cb0e7ff4a1b150164b4e
806-26 <806-26@DESKTOP-8V02G4F>
2024-04-19 18:17:59 +0900

    hi

--- src/com/practice/ex01/PracticeEx.java
+++ src/com/practice/ex01/PracticeEx.java
@@ -3,7 +3,7 @@ package com.practice.ex01;
 public class PracticeEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub//
        System.out.println("hi");
 	}
 
