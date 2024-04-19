7ec9d9aaa282f5632b0b03a0f1e7dcc36c53306d
806-26 <806-26@DESKTOP-8V02G4F>
2024-04-19 18:14:02 +0900

    first commit on 0419 18:13

---
+++ src/com/practice/ex01/PracticeEx.java
@@ -0,0 +1,10 @@
+package com.practice.ex01;
+
+public class PracticeEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hi");
+	}
+
+}
